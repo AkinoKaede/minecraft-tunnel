a56b60fe5437eee2c320920826bca3915f38229c
Kaede Akino <i@akinokae.de>
2026-06-24 18:37:28 +0800

    Remove native transport guard

--- forge/src/main/java/com/akinokaede/mctunnel/forge/MinecraftTunnelForgeMod.java
+++ forge/src/main/java/com/akinokaede/mctunnel/forge/MinecraftTunnelForgeMod.java
@@ -3,13 +3,11 @@ package com.akinokaede.mctunnel.forge;
 import com.akinokaede.mctunnel.MinecraftTunnel;
 import com.akinokaede.mctunnel.client.ClientEnvironment;
 import com.akinokaede.mctunnel.client.MinecraftTunnelClient;
-import com.akinokaede.mctunnel.transport.NettyNativeTransportGuard;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(MinecraftTunnel.MOD_ID)
 public final class MinecraftTunnelForgeMod {
 	public MinecraftTunnelForgeMod() {
-		NettyNativeTransportGuard.disableNativeTransportProbeOnNonLinux();
 		MinecraftTunnel.init();
 		if (ClientEnvironment.isClient()) {
 			MinecraftTunnelClient.init();
--- neoforge/src/main/java/com/akinokaede/mctunnel/neoforge/MinecraftTunnelNeoForgeMod.java
+++ neoforge/src/main/java/com/akinokaede/mctunnel/neoforge/MinecraftTunnelNeoForgeMod.java
@@ -3,13 +3,11 @@ package com.akinokaede.mctunnel.neoforge;
 import com.akinokaede.mctunnel.MinecraftTunnel;
 import com.akinokaede.mctunnel.client.ClientEnvironment;
 import com.akinokaede.mctunnel.client.MinecraftTunnelClient;
-import com.akinokaede.mctunnel.transport.NettyNativeTransportGuard;
 import net.neoforged.fml.common.Mod;
 
 @Mod(MinecraftTunnel.MOD_ID)
 public final class MinecraftTunnelNeoForgeMod {
 	public MinecraftTunnelNeoForgeMod() {
-		NettyNativeTransportGuard.disableNativeTransportProbeOnNonLinux();
 		MinecraftTunnel.init();
 		if (ClientEnvironment.isClient()) {
 			MinecraftTunnelClient.init();
